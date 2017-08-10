7463f7505bcc546c0fbb47daef8eea41a1b9ac2e
talesf <talsefzxc@gmail.com>
2017-08-10 22:06:18 +0800

    Add Material-ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+          <AppBar 
+            title="My AppBar" />
+        </MuiThemeProvider>
       </div>
     );
   }
